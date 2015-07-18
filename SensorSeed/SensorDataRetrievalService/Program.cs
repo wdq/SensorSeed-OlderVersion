6df03a963ce3aa5dfac85c9cc3f94ef59419526d
William Quade <liam@quade.co>
2015-07-18 17:50:16 -0500

    Initial commit.

---
+++ SensorSeed/SensorDataRetrievalService/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+
+namespace SensorDataRetrievalService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
